be0bcc2b301c81377cf732fcbfcf48a1a610f971
FlamedDogo99 <96555444+FlamedDogo99@users.noreply.github.com>
2024-06-08 22:11:55 -0600

    added version

--- eaglermobile.user.js
+++ eaglermobile.user.js
@@ -6,7 +6,7 @@
 // @downloadURL     https://raw.githubusercontent.com/FlamedDogo99/EaglerMobile/main/eaglermobile.user.js
 // @license         Apache License 2.0 - http://www.apache.org/licenses/
 // @match           https://eaglercraft.com/mc/*
-// @version         3.0.3-chat-fix
+// @version         3.0.4-chat-fix
 // @updateURL       https://raw.githubusercontent.com/FlamedDogo99/EaglerMobile/main/eaglermobile.user.js
 // @run-at          document-start
 // ==/UserScript==
