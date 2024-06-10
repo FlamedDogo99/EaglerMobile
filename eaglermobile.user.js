0d1e55f58849ba0ab71aed77b973ba8c2e7036f1
FlamedDogo99 <96555444+FlamedDogo99@users.noreply.github.com>
2024-06-09 19:12:00 -0600

    Update eaglermobile.user.js
    Added temporary DOM log for troubleshooting

--- eaglermobile.user.js
+++ eaglermobile.user.js
@@ -31,6 +31,28 @@ window.keyboardFix = false; // temporarily set to true until I can figure out wh
 var previousTouchX = null;
 var previousTouchY = null;
 var startTouchX = null;
+//TEMP REMOVE
+function logManager() {
+    var self = this;
+
+    self.init = function () {
+        console.log('logmanager initialized');
+        var old = console.log;
+        self.logger = document.getElementById('log');
+        console.log = function (message, options) {
+            if (typeof message == 'object') {
+                self.logger.innerHTML = (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />' + self.logger.innerHTML;
+            } else {
+                self.logger.innerHTML = message + '<br />' + self.logger.innerHTML;
+            }
+        }
+    }
+    self.toggleLogVisibility = function () {
+        return $(self.logger).toggle();
+    };
+}
+
+
 // better charCodeAt function
 String.prototype.toKeyCode = function() {
     const keyCodeList = {"0": 48, "1": 49, "2": 50, "3": 51, "4": 52, "5": 53, "6": 54, "7": 55, "8": 56, "9": 57, "backspace": 8, "tab": 9, "enter": 13, "shift": 16, "ctrl": 17, "alt": 18, "pause_break": 19, "caps_lock": 20, "escape": 27, " ": 32, "page_up": 33, "page_down": 34, "end": 35, "home": 36, "left_arrow": 37, "up_arrow": 38, "right_arrow": 39, "down_arrow": 40, "insert": 45, "delete": 46, "a": 65, "b": 66, "c": 67, "d": 68, "e": 69, "f": 70, "g": 71, "h": 72, "i": 73, "j": 74, "k": 75, "l": 76, "m": 77, "n": 78, "o": 79, "p": 80, "q": 81, "r": 82, "s": 83, "t": 84, "u": 85, "v": 86, "w": 87, "x": 88, "y": 89, "z": 90, "left_window_key": 91, "right_window_key": 92, "select_key": 93, "numpad_0": 96, "numpad_1": 97, "numpad_2": 98, "numpad_3": 99, "numpad_4": 100, "numpad_5": 101, "numpad_6": 102, "numpad_7": 103, "numpad_8": 104, "numpad_9": 105, "*": 106, "+": 107, "-": 109, ".": 110, "/": 111, "f1": 112, "f2": 113, "f3": 114, "f4": 115, "f5": 116, "f6": 117, "f7": 118, "f8": 119, "f9": 120, "f10": 121, "f11": 122, "f12": 123, "num_lock": 144, "scroll_lock": 145, ";": 186, "=": 187, ",": 188, "-": 189, ".": 190, "/": 191, "`": 192, "[": 219, "\\": 220, "]": 221, "\"": 222};
@@ -488,7 +510,7 @@ function insertCanvasElements() {
     }, false);
     document.body.appendChild(screenshotButton);
     let coordinatesButton = createTouchButton("coordinatesButton", "inGame");
-    coordinatesButton.style.cssText = "top: 0vh; margin: auto; left: 32vh; right: 0vh; width: 8vh; height: 8vh;"
+    coordinatesButton.style.cssText = ""
     coordinatesButton.addEventListener("touchstart", function(e) {
         keyEvent("f", "keydown");
         keyEvent("3", "keydown");
@@ -498,6 +520,15 @@ function insertCanvasElements() {
         keyEvent("3", "keyup");
     }, false);
     document.body.appendChild(coordinatesButton);
+    //TEMP REMOVE
+    var temp = document.createElement("div");
+    temp.id = "log";
+    temp.classList.add("log");
+    temp.style.cssText= "position:absolute; top: 8vh; margin: auto; left: 0vh; right:0vh";
+    temp.innerHtml = "Loading..."
+    document.body.appendChild(temp);
+    document.temp = new logManager();
+	document.temp.init();
 }
 // CSS for touch screen buttons, along with fixing iOS's issues with 100vh ignoring the naviagtion bar, and actually disabling zoom because safari ignores user-scalable=no :(
 let customStyle = document.createElement("style");
